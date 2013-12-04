bf6ae8895ad8f05083a0b031e2d8d928f1729234
Jovi Nel <jnp.parilla@gmail.com>
2013-12-04 11:01:49 +0800

    Added new files for the project
    Signed-off-by: Jovi Nel <jnp.parilla@gmail.com>

---
+++ Seg Nav/Seg Nav/BIDTaskDetailController.h
@@ -0,0 +1,13 @@
+//
+//  BIDTaskDetailController.h
+//  Seg Nav
+//
+//  Created by Maria Alice C. Lim on 12/4/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDTaskDetailController : UIViewController
+
+@end
