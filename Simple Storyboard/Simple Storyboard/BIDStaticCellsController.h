32184c2b9cde6460e1e663d2a9c233f24759d089
Jovi Nel <jnp.parilla@gmail.com>
2013-12-04 10:24:24 +0800

    Finished simple demo for storyboard and using static cells to represent static data.
    Signed-off-by: Jovi Nel <jnp.parilla@gmail.com>

---
+++ Simple Storyboard/Simple Storyboard/BIDStaticCellsController.h
@@ -0,0 +1,13 @@
+//
+//  BIDStaticCellsController.h
+//  Simple Storyboard
+//
+//  Created by Maria Alice C. Lim on 12/4/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDStaticCellsController : UITableViewController
+
+@end
